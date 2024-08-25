2a409c2d1b900d1b9b06c4ea4ff7902860ab6b18
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2024-08-25 16:30:53 +0300

    beginning

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,6 +1,6 @@
 import s from "./ErrorMessage.module.css";
 
-const ErrorMessage = () => {
+const ErrorMessage: React.FC = () => {
   return (
     <div className={s.error}>
       <p>Something went wrong! Try again...</p>
